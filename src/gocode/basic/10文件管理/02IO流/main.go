c7180d40cbf45e12a561a8675cdb9bbe341e92b7
AccessOK <accessok@163.com>
2024-12-10 12:44:21 +0800

    add io

--- src/gocode/basic/10文件管理/02IO流/main.go
+++ src/gocode/basic/10文件管理/02IO流/main.go
@@ -31,4 +31,28 @@ func main(){
 		fmt.Println(str)
 	}
 	fmt.Println("ending")
+	//写入文件
+	file1,err1 := os.OpenFile("D:/AccessOK/code/golang/src/gocode/basic/10文件管理/test.txt",os.O_RDWR|os.O_APPEND,0666)
+	if err1 != nil {
+		fmt.Println("打开失败")
+		return
+	}
+	defer file1.Close()
+	writer := bufio.NewWriter(file1)
+	for i := 0; i < 10; i++ {
+		writer.WriteString("\n你好，这是输入流")
+	}
+	writer.Flush()//流数据在缓冲区中，刷新，将缓冲区中的内容保存到磁盘
+	//文件复制
+	//源文件
+	content1,err1 := ioutil.ReadFile("D:/AccessOK/code/golang/src/gocode/basic/10文件管理/test.txt")
+	if err != nil {
+		fmt.Println("读取失败")
+		return
+	}
+	//目标文件
+	err2 := ioutil.WriteFile("D:/AccessOK/code/golang/src/gocode/basic/10文件管理/test1.txt",content1,0666)
+	if err2 != nil{
+		fmt.Println("写入失败！")
+	}
 }
