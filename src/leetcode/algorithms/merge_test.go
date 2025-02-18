4e0f6b26dd1de53895dc95f25567cc23a20d815f
AccessOK <accessok@163.com>
2025-02-18 14:11:23 +0800

    add leetcode

--- src/leetcode/algorithms/merge_test.go
+++ src/leetcode/algorithms/merge_test.go
@@ -5,6 +5,5 @@ import "testing"
 func TestMerge(t *testing.T) {
 	var num1 = []int{0, 1, 2, 3, 0, 0, 0}
 	var num2 = []int{2, 4, 5}
-	println("wangjing")
-	merge(num1, 3, num2, 3)
+	merge(num1, 4, num2, 3)
 }
