37b84ddb369446cca842f1128eeae676ca60fd33
AccessOK <accessok@163.com>
2025-03-13 00:45:16 +0800

    rotate

--- src/leetcode/algorithms/rotate_test.go
+++ src/leetcode/algorithms/rotate_test.go
@@ -3,6 +3,7 @@ package algorithms
 import "testing"
 
 func TestRotate(t *testing.T) {
-	nums := []int{1, 2, 3}
-	rotate(nums, 2)
+	nums := []int{1, 2, 3, 4, 5, 6, 7}
+	//nums := []int{1, 2, 3}
+	rotate(nums, 5)
 }
